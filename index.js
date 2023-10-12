cb8653cd8d5855af30498d202f43a1838087e74d
imananuar <imananuar5367@gmail.com>
2023-10-12 23:55:18 +0800

    Websocket server init

--- index.js
+++ index.js
@@ -16,8 +16,21 @@ const io = new Server(server, {
 app.get('/', (req, res) => res.send("Hello World")); 
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    const id = socket.id;
+    socket.emit("id", id);
+    console.log(`User connected: ${id}`);
+    socket.on("message", (data) => {
+        console.log(`Get data from client: ${id}: ${data.message}`)
+        socket.broadcast.emit("test", {message: data.message, user: id})
+    })
 });
+
+
+io.on('disconnect', (socket) => {
+    console.log("User disconnect from server");
+    io.emit("disconnect!");
+    socket.broadcast.emit("hello", "world"); 
+})
 
 server.listen(9876, () => {
     console.log('Listening on port 9876');
